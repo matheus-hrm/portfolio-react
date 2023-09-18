65ed3dd19ded509b9970133d67101344524a6628
matheus-hrm <rodriguesm673@gmail.com>
2023-09-18 00:32:46 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { Theme } from '@radix-ui/themes'
+import { Button } from '@radix-ui/themes'
+import { Moon } from 'lucide-react'
 
 export function App() {
   return (
-    <header className='min-h-screen flex flex-row px-6 py-4 justify-evenly' >
-      <div className='flex  '>
-        <div className='px-4'> Sobre mim </div>
-        <div className='px-4'> Projetos  </div>
-        <div className='px-4'> Contato  </div>
-      </div>
-    </header>
+    <Theme 
+    appearance='dark' 
+    accentColor='iris'
+    radius='full'
+    >
+    <section className='min-h-screen flex flex-row px-6 py-4 justify-evenly border-b' >
+      <nav className='flex flex-row justify-evenly w-full h-full' >
+        <h1 className='px-4'> Sobre mim </h1>
+        <h1 className='px-4'> Projetos  </h1>
+        <h1 className='px-4'> Contato  </h1>
+        <Button variant='outline' radius='full'>
+          <Moon />
+        </Button>
+        
+      </nav>
+    </section>
+    </Theme>
         
   )
 }
